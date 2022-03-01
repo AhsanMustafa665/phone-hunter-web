36d123af2f16222bb5d8c75a35759abb49e37546
AhsanMustafa665 <ahsanfaisal665@gmail.com>
2022-03-01 23:21:22 +0600

    api name mistake show error message

--- js/app.js
+++ js/app.js
@@ -1,37 +1,81 @@
+document.getElementById('error-message').style.display = 'none';
+
+
 const searchButton = () => {
     const searchFeild = document.getElementById('search-feild')
     const searchText = searchFeild.value;
     // console.log(searchText);
-    // searchFeild.value = ""
 
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    // console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(data => ShowSearchResult(data.data))
+    // clear data
+    searchFeild.value = ""
+    document.getElementById('error-message').style.display = 'none';
+    if (searchText == '') {
+        // alert('asdf')
+
+    }
+
+    // load data
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        // console.log(url);
+        fetch(url)
+            .then(res => res.json())
+            .then(data => ShowSearchResult(data.data))
+            .catch(error => displayError(error))
+    }
+}
+const displayError = error => {
+    document.getElementById('error-message').style.display = 'block';
 }
 const ShowSearchResult = (data) => {
     // console.log(data);
     const searchResult = document.getElementById('search-result')
-    data.forEach(byte => {
-        // console.log(byte);
+    const first20data = data.slice(0, 20);
+    // clear info(image and others)
+    searchResult.textContent = ''
+    first20data.forEach(byte => {
+        console.log(byte);
         const div = document.createElement('div')
         div.classList.add('col');
         div.innerHTML = `
-        <div onclick="loadPhoneDetail('${byte.slug}')" class="card w-75 h-100 bg-dark round ">
+        <div class="card w-75 h-100 bg-dark round ">
             <img class="w-100 round2" src="${byte.image}" class="card-img-top" alt="...">
             <div id="search-result" class="card-body px-4 py-4 ">
                 <h3 class="card-title text-primary">Phone Name : ${byte.phone_name}</h3>
                 <h4 class="text-info pt-2">Phone Brand : ${byte.brand}</h4>
-            </div>
+                <button onclick="loadPhoneDetail('${byte.slug}')" href="#" class="btn btn-primary text-text-center mt-3 mx-5">More Explore</buttonhref=>
+        </div>
+            
       </div>`;
         searchResult.appendChild(div)
     })
 }
 const loadPhoneDetail = (phoneSlug) => {
     // console.log(phoneSlug);
-    const url = `https://openapi.programming-hero.com/api/phone/oppo_find_x5_pro-11236`
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneSlug}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => showPhoneDetail(data.data))
+}
+
+const showPhoneDetail = (info) => {
+    // console.log(info);
+    const phoneDetails = document.getElementById('phone-details')
+    phoneDetails.textContent = '';
+    const div = document.createElement('div')
+    div.classList.add('card');
+    div.innerHTML = `
+    <div class="card w-100 bg-warning round ">
+    <img src="${info.image}" class="card-img-top" alt="...">
+        <div class="card-body details-bg my-3 ">
+          <h4 class="card-title mb-4 text-primary">Release Date : ${info.releaseDate}</h4>
+          <p><span class="fw-bold fst-italic ">Storage :</span> ${info.mainFeatures.storage}</p>
+          <p><span class="fw-bold fst-italic">Display-size :</span> ${info.mainFeatures.displaySize}</p>
+          <p><span class="fw-bold fst-italic">Chipset :</span> ${info.mainFeatures.chipSet}</p>
+          <p><span class="fw-bold fst-italic">Memory :</span> ${info.mainFeatures.memory}</p>
+          
+        </div>
+        </div>
+    `;
+    phoneDetails.appendChild(div)
 }
