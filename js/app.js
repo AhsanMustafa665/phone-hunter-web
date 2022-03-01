a9ecd450b80894a5803b800b4132d19156825260
AhsanMustafa665 <ahsanfaisal665@gmail.com>
2022-03-01 11:25:14 +0600

    added card img-phone-brand

--- js/app.js
+++ js/app.js
@@ -10,3 +10,21 @@ const searchButton = () => {
         .then(res => res.json())
         .then(data => ShowSearchResult(data.data))
 }
+const ShowSearchResult = (data) => {
+    // console.log(data);
+    const searchResult = document.getElementById('search-result')
+    data.forEach(byte => {
+        // console.log(byte);
+        const div = document.createElement('div')
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick="loadPhoneDetail('${byte.slug}')" class="card w-75 h-100 bg-dark round ">
+            <img class="w-100 round2" src="${byte.image}" class="card-img-top" alt="...">
+            <div id="search-result" class="card-body px-4 py-4 ">
+                <h3 class="card-title text-primary">Phone Name : ${byte.phone_name}</h3>
+                <h4 class="text-info pt-2">Phone Brand : ${byte.brand}</h4>
+            </div>
+      </div>`;
+        searchResult.appendChild(div)
+    })
+}
